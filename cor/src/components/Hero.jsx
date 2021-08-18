7fec624f1c105a6dc7d0b243e2afb7afd77224e9
chris <c.caressimo@gmail.com>
2021-08-17 20:11:44 -0400

    scroll

--- cor/src/components/Hero.jsx
+++ cor/src/components/Hero.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { NavLink } from "react-router-dom"
 import { Link } from 'react-scroll'
 import "./css/Hero.css"
-
-import Impact from "./Impact"
-
 
 export default function Hero() {
     return (
